d4191f057acea45dd5413f616fe23da87f99867f
Apartamentos Tío José María <apartamentostiojosemaria@gmail.com>
2026-03-04 09:05:59 +0100

    fix: allow easypanel host in vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-})
+    server: {
+        host: true
+    },
+    preview: {
+        host: true,
+        allowedHosts: [
+            "nuevo-web-padron-ia.3pkgp0.easypanel.host"
+        ]
+    }
+});
